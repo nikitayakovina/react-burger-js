e7ba94c65e72393cc1f5e4fd19d9145f767bf46d
nikitayakovina <36497484+nikitayakovina@users.noreply.github.com>
2025-09-03 22:28:02 +0600

    fix ts

--- src/utils/checkResponse.ts
+++ src/utils/checkResponse.ts
@@ -1,3 +1,3 @@
-export const checkResponse = <T>(data): Promise<T> => {
+export const checkResponse = <T>(data: Response): Promise<T> => {
   return data.ok ? data.json() : data.json().then((err) => Promise.reject(err));
 };
